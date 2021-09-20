2c16b9489648b8d64162e40edc1e1b03428a9628
jacobharmon43 <64942817+jacobharmon43@users.noreply.github.com>
2021-09-19 20:15:27 -0500

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -23,4 +23,19 @@ var toggle = function (elem) {
     show(elem);
 }
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for(i = 0; i < coll.length; i++){
+    coll[i].addEventListener("click", function() {
+        var content = this.nextElementSibling;
+        if(content.style.display === "block"){
+            content.style.display = "none";
+        }
+        else{
+            content.style.display = "block";
+        }
+    });
+}
+
 
